04c7888935a2a01dd61e8536b4269e0e45edc891
Michael Haberler <git@mah.priv.at>
2025-07-17 14:49:01 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pmtiles-dem-lookup',
+  webDir: 'dist'
+};
+
+export default config;
